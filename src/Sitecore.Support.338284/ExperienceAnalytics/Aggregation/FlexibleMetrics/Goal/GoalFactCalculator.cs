c8671e4899eeb02872625ff921f03684f4837435
Sergey Plashenko <sergey.plashenko@sitecore.com>
2019-06-22 14:41:55 +0300

    File from dotpeek.

---
+++ src/Sitecore.Support.338284/ExperienceAnalytics/Aggregation/FlexibleMetrics/Goal/GoalFactCalculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Sitecore.Analytics.Aggregation;
+using Sitecore.ExperienceAnalytics.Aggregation.FlexibleMetrics.Framework.FactCalculation;
+using Sitecore.ExperienceAnalytics.Aggregation.FlexibleMetrics.Framework.Grouping;
+
+namespace Sitecore.Support.ExperienceAnalytics.Aggregation.FlexibleMetrics.Goal
+{
+    internal class GoalFactCalculator : IFactCalculator<GoalMetrics, XConnect.Goal>
+    {
+        public GoalMetrics CalculateFactsForGroup(
+            VisitGroupMeasurement<XConnect.Goal> groupMeasurement,
+            IInteractionAggregationContext context)
+        {
+            if (groupMeasurement == null)
+            {
+                throw new ArgumentNullException(nameof(groupMeasurement));
+            }
+
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return new GoalMetrics
+            {
+                Visits = 1,
+                Value = context.Interaction.EngagementValue,
+                Conversions = context.Interaction.Events.Count(evt => evt is XConnect.Goal),
+                Count = groupMeasurement.Occurrences.Count()
+            };
+        }
+    }
+}
---
+++ src/Sitecore.Support.338284/ExperienceAnalytics/Aggregation/FlexibleMetrics/Goal/GoalMetrics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Sitecore.Analytics.Aggregation.Data.Model;
+using Sitecore.ExperienceAnalytics.Aggregation.FlexibleMetrics.Framework.Metrics;
+
+namespace Sitecore.Support.ExperienceAnalytics.Aggregation.FlexibleMetrics.Goal
+{
+    internal class GoalMetrics : DictionaryValue, IMergeableMetric<GoalMetrics>
+    {
+        public int Visits { get; set; }
+
+        public int Value { get; set; }
+
+        public int Conversions { get; set; }
+
+        public int Count { get; set; }
+
+        public GoalMetrics MergeWith(GoalMetrics other)
+        {
+            if (other == null)
+            {
+                throw new ArgumentNullException(nameof(other));
+            }
+
+            return new GoalMetrics
+            {
+                Visits = Visits += other.Visits,
+                Value = Value += other.Value,
+                Conversions = Conversions += other.Conversions,
+                Count = Count += other.Count
+            };
+        }
+    }
+}
